5528be8561d89be15a991e5632f00faee36c64a6
lilei <leeoohoo@gmail.com>
2026-09-23 17:13:01 +0800

    fix(windows): use runtime bindings in pet templates

--- clients/windows/src/ChatOS.Desktop/Features/Pet/PetWindow.xaml.cs
+++ clients/windows/src/ChatOS.Desktop/Features/Pet/PetWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Runtime.InteropServices;
 using ChatOS.Connector.Approval;
 using ChatOS.Core.Abstractions;
 using ChatOS.Core.Domain;
-using ChatOS.Presentation.Chat;
 using ChatOS.Presentation.Pet;
 using Microsoft.UI.Input;
 using Microsoft.UI.Windowing;
@@ -462,14 +461,6 @@ public sealed partial class PetWindow : Window
     }
 
     private async void OnCancelTaskClicked(object sender, RoutedEventArgs e) => await ViewModel.CancelSelectedAsync();
-
-    private void OnAskUserSecretChanged(object sender, RoutedEventArgs e)
-    {
-        if (sender is PasswordBox { DataContext: AskUserFieldInputViewModel field } passwordBox)
-        {
-            field.Value = passwordBox.Password;
-        }
-    }
 
     private async void OnDeclineApproval(object sender, RoutedEventArgs e) =>
         await ResolveApprovalAsync(ConnectorApprovalAction.Decline);
